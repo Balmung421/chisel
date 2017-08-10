9c8e2fae9ceec8dd60ee8d817b83409c05268ff7
Balmung421 <zcook421@gmail.com>
2017-08-09 20:21:23 -0600

    Added passing reading input md file test

--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -4,14 +4,19 @@ require './lib/chisel'
 
 class ChiselTest < Minitest::Test
 
-  attr_reader :chisel
+  attr_reader :chisel, :input
 
   def setup
     @chisel = Chisel.new(File.open('input.md', "r"),  File.open('output.html', "w"))
+    @input = File.open('input.md', "r").read
   end
 
   def test_if_chisel_exists
     assert_instance_of Chisel, chisel
+  end
+
+  def test_it_reads_the_file
+    assert true, input
   end
 
 end
