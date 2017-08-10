2888ff3c95eb8f9732a63b13e8f5f0582a8b5dca
Balmung421 <zcook421@gmail.com>
2017-08-09 20:24:36 -0600

    Passing empty output file

--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -4,11 +4,12 @@ require './lib/chisel'
 
 class ChiselTest < Minitest::Test
 
-  attr_reader :chisel, :input
+  attr_reader :chisel, :input, :output
 
   def setup
     @chisel = Chisel.new(File.open('input.md', "r"),  File.open('output.html', "w"))
     @input = File.open('input.md', "r").read
+    @output = File.open('output.html', "w")
   end
 
   def test_if_chisel_exists
@@ -17,6 +18,10 @@ class ChiselTest < Minitest::Test
 
   def test_it_reads_the_file
     assert true, input
+  end
+
+  def test_the_output_file_is_empty_and_can_be_written_to
+    assert true, File.zero?(output)
   end
 
 end
