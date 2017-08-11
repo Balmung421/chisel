9696f949ca98fc9bca9482e36ba6303b1ca6ee25
Balmung421 <zcook421@gmail.com>
2017-08-11 17:29:55 -0600

    Refactor chisel

--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -2,35 +2,45 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/chisel'
 
-class ChiselTest < Minitest::Test
-
-  attr_reader :chisel, :input, :output, :renderer
+class ChiselTest < MiniTest::Test
+  attr_reader :chisel
 
   def setup
-    @chisel = Chisel.new(File.open('input.md', "r"),  File.open('output.html', "w"))
-    @input = File.open('input.md', "r").read
-    @output = File.open('output.html', "w")
-    # @renderer = Render.new(@input)
+    @chisel = Chisel.new('./testfiles/chisel.txt', './testfiles/tester.txt')
   end
 
-  def test_if_chisel_exists
-    assert_instance_of Chisel, chisel
+  def test_it_can_take_in_a_file_to_read
+    assert_equal './testfiles/chisel.txt', chisel.markdown_file
   end
 
-  def test_it_reads_the_file
-    assert true, input
+  def test_it_can_take_in_a_file_to_write
+    assert_equal './testfiles/tester.txt', chisel.html_file
   end
 
-  def test_the_output_file_is_empty_and_can_be_written_to
-    assert true, File.zero?(output)
+  def test_it_will_default_to_nil_if_no_html_file_is_given
+    chisel = Chisel.new('./testfiles/chisel.txt')
+    assert_equal nil, chisel.html_file
   end
 
-  def test_it_sends_input_to_renderer
-    assert true, chisel.respond_to?(:call)
+  def test_it_can_call_the_emphasis_class_methods
+    emph = Emphasizer.new('./testfiles/tester.txt')
+    assert emph
   end
 
-  def test_it_writes_the_return_value_to_the_output_file
-    assert_equal Fixnum, chisel.call.class
+  def test_it_will_split_the_text_into_chunks_on_two_returns
+    chisel = Chisel.new("this is\n a test\n\n string")
+    assert_equal ["this is\n a test", " string"], chisel.split_text
   end
 
+  def test_it_parses_paragraphs
+    chisel = Chisel.new("This is a paragraph")
+    assert_equal "<p>\nThis is a paragraph\n</p>\n", chisel.parse
+  end
+
+  def test_it_parses_an_entire_document
+    correct_parse = File.read './testfiles/parsed-example.txt'
+    chisel.parse
+    result = File.read "./testfiles/parsed.txt"
+    assert_equal correct_parse, result
+  end
 end
