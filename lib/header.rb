e432486ccdf8a9bf8e540710110011e2ddd51039
Balmung421 <zcook421@gmail.com>
2017-08-11 16:02:17 -0600

    Header implemented

--- lib/header.rb
+++ lib/header.rb
@@ -1,4 +1,24 @@
-
 class Header
 
+  def initialize(text)
+    @text = text
+    @words = @text.split
+  end
+
+  def call
+    rendered_with_heading_tags
+  end
+
+  def rendered_with_heading_tags
+    "<h#{counter}>#{stripped_of_markdown}</h#{counter}>"
+  end
+
+  def counter
+    @counter ||= @words.first.count("#")
+  end
+
+  def stripped_of_markdown
+    @words.shift
+    @words.join(" ")
+  end
 end
--- test/header_test.rb
+++ test/header_test.rb
@@ -4,5 +4,38 @@ require './lib/header'
 
 class HeaderTest < Minitest::Test
 
+  def test_it_counts_the_tags
+    input_text = "## This is a heading"
+    expected = 2
 
+    assert_equal expected, Header.new(input_text).counter
+  end
+
+  def test_it_strips_the_text_of_any_markdown
+    input_text = "## This is a heading"
+    expected = "This is a heading"
+
+    assert_equal expected, Header.new(input_text).stripped_of_markdown
+  end
+
+  def test_it_wraps_an_h1_tag_around_a_header
+    input_text = "# This is a heading"
+    expected_text = "<h1>This is a heading</h1>"
+
+    assert_equal expected_text, Header.new(input_text).call
+  end
+
+    def test_it_counts_and_adds_tags_for_h3
+    input_text = "### This is an h3 heading."
+    expected_text = "<h3>This is an h3 heading.</h3>"
+
+    assert_equal expected_text, Header.new(input_text).call
+  end
+
+   def test_it_counts_and_adds_tags_for_h4
+    input_text = "#### This is an h4 heading."
+    expected_text = "<h4>This is an h4 heading.</h4>"
+
+    assert_equal expected_text, Header.new(input_text).call
+  end
 end
