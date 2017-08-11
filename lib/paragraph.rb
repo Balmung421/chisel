875076862325cb419971384734dcc9f315514743
Balmung421 <zcook421@gmail.com>
2017-08-11 16:07:29 -0600

    Pushing what I have before refactor/fixes

--- lib/paragraph.rb
+++ lib/paragraph.rb
@@ -1,3 +1,18 @@
 class Paragraph
 
+  def initialize(text)
+    @text = text
+  end
+
+  def call
+    <<-eos
+<p>
+    #{format_linebreaks}
+</p>
+eos
+  end
+
+  def format_linebreaks
+    @text.chomp.gsub("\n", "<br>\n    ")
+  end
 end
--- lib/render.rb
+++ lib/render.rb
@@ -1,5 +1,11 @@
-require_relative 'header'
 require_relative 'paragraph'
+require_relative 'header'
+require_relative 'emphasize'
+require_relative 'strong'
+require_relative 'list'
+require_relative 'links'
+require_relative 'image'
+
 
 class Render
 
@@ -12,7 +18,48 @@ class Render
   end
 
   def call
-
+    links = links(@text)
+    strong = strong(links)
+    emphasized = emphasize(strong)
+    # image = image(image)
+    final = basic_formatting(emphasized.split("\n\n"))
   end
 
+  def basic_formatting(text)
+    text.map do |chunk|
+      if chunk[0] == "#"
+        Header.new(chunk).call
+      elsif chunk[0] == "*"
+        UnorderedList.new(chunk).call
+      elsif chunk[0] == "1"
+        OrderedList.new(chunk).call
+      else
+        Paragraph.new(chunk).call
+      end
+    end.join("\n")
+  end
+
+  def strong(text)
+    if text.include?("**")
+      Strong.new(text).call
+    else
+      text
+    end
+  end
+
+  def emphasize(text)
+    if text.include?("*") && text[0] != "*"
+      Emphasize.new(text).call
+    else
+      text
+    end
+  end
+
+  def links(text)
+    if text.include?("](")
+      Links.new(text).call
+    else
+      text
+    end
+  end
 end
