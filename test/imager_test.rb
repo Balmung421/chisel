6bb576f3fd473cb42adce3b2b3630fae72b3b0ee
Balmung421 <zcook421@gmail.com>
2017-08-11 20:30:48 -0600

    Image tests added

---
+++ test/imager_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/imager'
+
+class ImagerTest < MiniTest::Test
+  attr_reader :imager
+
+  def setup
+    @imager = Imager.new
+  end
+
+  def test_it_can_take_a_string
+    expected = "<p><img src=interspace-starcadian.jpg>Starcadian</img></p>\n"
+    assert_equal expected, imager.image_replacer("![Starcadian](interspace-starcadian.jpg)")
+  end
+
+  def test_no_alt_text
+    expected = "<p><img src= interspace-starcadian.jpg></img></p>\n"
+    assert_equal expected, imager.image_replacer("![](interspace-starcadian.jpg)")
+  end
+
+end
